6cb62856f5ab4bd3c15f764f1a840d6af92d65f4
Sulaiman-kc <sulaimankc9@gmail.com>
2019-02-09 15:39:31 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <?php 
+
+
+
+
+    ?>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<ul class=nav-list>
+<li><a href="#">home</a> </li>
+<li>about</li>
+<li>contact us</li>
+<li>login</li>
+
+</ul>
+
+
+</div>
+<div class="row">anb</div>
+<div class="row">footer</div>
+
+
+</div>
+</body>
+</html>
